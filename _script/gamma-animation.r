c3b96dfb26305d4d079bed60c036fd1cdbd426ef
Raju Rimal <therimalaya@gmail.com>
2018-03-13 18:33:49 +0100

    added few slides

--- _script/gamma-animation.r
+++ _script/gamma-animation.r
@@ -3,9 +3,10 @@ library(tidyverse)
 
 # Multicollinearity
 dta <- map_df(1:15, ~tibble(
-  gamma = c(seq(0.1, 0.5, length.out = 6), seq(0.6, 1.2, length.out = 4)),
-  lambda = exp(-1 * gamma * (.x - 1))
-), .id = "variable") %>% 
+  gamma = c(seq(0.1, 0.5, length.out = 6), 
+            seq(0.6, 1.2, length.out = 4)),
+  lambda = exp(-1 * gamma * (.x - 1))), 
+  .id = "variable") %>% 
   mutate(variable = as.numeric(variable))
 
 static_plt <- dta %>% 
@@ -25,11 +26,16 @@ static_plt <- dta %>%
 ggsave(static_plt, filename = "_images/multicollinearity.pdf",
        dpi = 300, width = 7, height = 7)
 
-plt <- ggplot(dta, aes(variable, lambda, frame = paste("Degree of multicollinearity:", round(gamma, 2)))) +
-  geom_bar(stat = "identity", position = "identity", fill = "lightgrey") +
+plt <- ggplot(dta, aes(
+  variable, lambda, 
+  frame = paste("Degree of multicollinearity:", round(gamma, 2)))) +
+  geom_bar(stat = "identity", 
+           position = "identity", 
+           fill = "lightgrey") +
   geom_point() + geom_line(group = 1) +
   labs(x = "Variable", y = "Eigenvalues") +
   scale_x_continuous(breaks = unique(dta$variable)) +
+  theme_bw() +
   theme(text = element_text(size = 18)) +
   annotate(label = "lambda[i] == e^-gamma(x[i] - 1)", 
            size = 12, family = "serif", geom = "text",
@@ -38,5 +44,4 @@ plt <- ggplot(dta, aes(variable, lambda, frame = paste("Degree of multicollinear
            size = 12, family = "serif", geom = "text",
            x = Inf, y = Inf, hjust = 1, vjust = 2, parse = TRUE)
   
-gganimate(plt, "_images/multicollinearity.gif", ani.dev = "png", 
-          res = 150, width = 600, height = 600)
+gganimate(plt, "_images/multicollinearity.gif", ani.dev = "png")
